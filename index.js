a32c279c3c92df964fd58c116266873ec0e893de
Cara Morelli <caramorelli7@gmail.com>
2018-11-18 23:38:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution in this file!
+
+const driver = { };
+
+function updateDriverWithKeyAndValue(driver, key, value) {
+  var new_obj = {};
+  for (var keys in driver) {
+    var val = driver[keys];
+    new_obj[keys] = val;
+  }
+  new_obj[key] = value;
+  return new_obj;
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+  driver[key] = value;
+  return driver;
+}
+
+function deleteFromDriverByKey(driver, key) {
+  var new_obj = {};
+  for (var keys in driver) {
+    var val = driver[keys];
+    if (keys !== key) {
+      new_obj[keys] = val;
+    }
+  }
+  return new_obj;
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key) {
+  delete driver[key];
+  return driver;
+}
